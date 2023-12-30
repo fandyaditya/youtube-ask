0de88012e467cd0b1fd6a43481e7cebc29afe1b5
I Made Fandy Aditya Wirana <fandyadityaw@gmail.com>
2023-12-30 08:51:17 +0800

    New commit

---
+++ index.js
@@ -0,0 +1,14 @@
+//import mindsdb es6
+import { getInstance } from './mindsdb';
+
+
+(async () => {
+    const mindsdb = await getInstance();
+    
+    if (!mindsdb) {
+        console.log('Failed to connect to mindsdb')
+        return;
+    }
+
+
+})()
